ea4e5c91a504657a439c8b91b207d1ec71a7f01d
Hanbaek Lyu <hlyu@math.wisc.edu>
2022-02-09 11:49:29 -0600

    Bump version: 0.0.1 → 0.1.0

--- src/NNetwork/__init__.py
+++ src/NNetwork/__init__.py
@@ -2,7 +2,7 @@
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 __author__ = "Hanbaek Lyu"
 __email__ = "hlyu@math.ucla.edu"
